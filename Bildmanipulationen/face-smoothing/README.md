1c1be81e15527aefe804ccfc77031f70dd0c72d0
Jan Noel Stricker <jannoel07@web.de>
2025-08-15 19:18:17 +0200

    Add Bildmanipulationen

---
+++ Bildmanipulationen/face-smoothing/README.md
@@ -0,0 +1,46 @@
+[![Build Status](https://travis-ci.com/5starkarma/face-smoothing.svg?branch=main)](https://travis-ci.com/5starkarma/face-smoothing) [![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)
+
+
+# Face Smoothing: Detection and Beautification
+
+Input Image             |  Output Image w/ Facial Smoothing
+:-------------------------:|:-------------------------:
+![alt text](https://github.com/5starkarma/face-smoothing/blob/main/data/images/hillary_clinton.jpg?raw=true "Input image")  |  ![alt text](https://github.com/5starkarma/face-smoothing/blob/main/data/output/output_0.jpg?raw=true "Output image")
+---
+OpenCV implementation of facial smoothing. Facial detection is done using an pretrained TensorFlow face detection model. Facial smoothing is accomplished using the following steps:
+
+- Change image from BGR to HSV colorspace
+- Create mask of HSV image
+- Apply a bilateral filter to the Region of Interest
+- Apply filtered ROI back to original image
+
+---
+
+## Install
+```
+git clone https://github.com/5starkarma/face-smoothing.git
+cd face-smoothing
+```
+## Run
+```
+python3 infer.py --input 'path/to/input_file.jpg' (Input file - image, video, or folder with images and/or videos - default is hillary_clinton.jpg)
+                         'can/handle/videos.mp4'
+                         'as/well/as/directories'
+                 --output 'path/to/output_folder' (Output folder - default is data/output)
+                 --save_steps 'path/to/file.jpg' (Concats images from each step of the process and saves them)
+                 --show-detections (Saves bounding box detections to output)
+```
+#### Example: --save-steps flag
+![alt text](https://github.com/5starkarma/face-smoothing/blob/main/data/output/combined_0.jpg?raw=true "Processing steps")
+
+## TODO
+- [X] Finish documentation and cleanup functions
+- [X] Reduce input image size for detections
+- [X] Fix combined output
+- [X] Test on multiple faces
+- [X] Apply blurring on multiple faces
+- [X] Video inference
+- [X] Save bounding box to output
+- [ ] Apply different blurring techniques/advanced algo using facial landmarks to blur only skin regions
+- [ ] Unit tests
+- [ ] Run time tests on units
---
+++ Bildmanipulationen/face-smoothing/infer.py
@@ -0,0 +1,167 @@
+import os
+import argparse
+import yaml
+import time
+
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+
+from detector.detect import detect_face
+from detector.smooth import smooth_face
+from utils.image import (load_image, 
+                         save_image, 
+                         save_steps, 
+                         check_img_size,
+                         get_height_and_width,
+                         process_image,
+                         check_if_adding_bboxes)
+from utils.video import (split_video,
+                         process_video)
+from utils.types import (is_image,
+                         is_video,
+                         is_directory)
+
+
+def parse_args():
+    """
+    Argument parser for cli.
+
+    Returns
+    -------
+    args : ArgumentParser object
+        Contains all the cli arguments
+    """
+    parser = argparse.ArgumentParser(description='Facial detection and \
+                                     smoothing using OpenCV.')
+    parser.add_argument('--input', 
+                        type=str, 
+                        help='Input file or folder',
+                        default='data/images/hillary_clinton.jpg')
+    parser.add_argument('--output', 
+                        type=str, 
+                        help='Output file or folder',
+                        default='data/output')
+    parser.add_argument('--show-detections', 
+                        action='store_true',
+                        help='Displays bounding boxes during inference.')
+    parser.add_argument('--save-steps', 
+                        action='store_true',
+                        help='Saves each step of the image.')
+    args = parser.parse_args()
+    # assert args.image_shape is None or len(args.image_shape) == 2, \
+    #     'You need to provide a 2-dimensional tuple as shape (H,W)'
+    # assert (is_image(args.input) and is_image(args.output)) or \
+    #        (not is_image(args.input) and not is_image(args.input)), \
+    #     'Input and output must both be images or folders'
+    return args
+
+
+def load_configs():
+    """
+    Loads the project configurations.
+
+    Returns
+    -------
+    configs : dict
+        A dictionary containing the configs
+    """
+    with open('configs/configs.yaml', 'r') as file:
+        return yaml.load(file, Loader=yaml.FullLoader)
+    
+def save_image(filename, image):
+    """
+    Speichert ein Bild so, dass der erste Dateiname ohne Zahl ist.
+    Falls die Datei bereits existiert, wird ein Zähler angehängt.
+    """
+    base, ext = os.path.splitext(filename)
+    if not ext:
+        ext = ".jpg"
+
+    counter = 0
+    save_path = base + ext
+    while os.path.exists(save_path):
+        counter += 1
+        save_path = f"{base}{counter}{ext}"
+	output_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    cv2.imwrite(save_path, output_rgb)
+    return save_path
+
+
+def main(args):
+    """Puts it all together."""
+    # Start measuring time
+    tic = time.perf_counter()
+    # Load project configurations
+    cfg = load_configs()
+    # Load the network
+    net = cv2.dnn.readNetFromTensorflow(cfg['net']['model_file'], 
+                                        cfg['net']['cfg_file'])
+    # Input and load image
+    input_file = args.input
+
+    try:
+        # If file is a compatible video file
+        if is_video(input_file):
+            # Process video
+            process_video(input_file, args, cfg, net)
+
+        # If file is a compatible image file
+        elif is_image(input_file):
+            # Load image
+            input_img = load_image(input_file)
+            # Process image
+            img_steps = process_image(input_img, cfg, net)
+            # Save final image to specified output filename
+            base_name = os.path.splitext(os.path.basename(input_file))[0]  # ohne Endung
+            out_filename = os.path.join(args.output, f"{base_name}_bf")  # keine Endung hier
+            # Check for --show-detections flag
+            output_img = check_if_adding_bboxes(args, img_steps)
+            # Save image
+            img_saved = save_image(out_filename, output_img)
+
+        # If input_file is a dir
+        elif is_directory(input_file):
+            # For each file in the dir
+            for file in os.listdir(input_file):
+                # Join input dir and file name
+                file = os.path.join(input_file, file)
+                # If file is a compatible video file
+                if is_video(file):
+                    # Process video
+                    process_video(file, args, cfg, net)
+                # If file is a compatible image file    
+                if is_image(file):
+                    # Load image
+                    input_img = load_image(file)
+                    # Process image
+                    img_steps = process_image(input_img, cfg, net)
+                    # Save final image to specified output filename
+                    base_name = os.path.splitext(os.path.basename(file))[0]  # ohne Endung
+                    out_filename = os.path.join(args.output, f"{base_name}_bf")  # keine Endung hier
+                     # Check for --show-detections flag
+                    output_img = check_if_adding_bboxes(args, img_steps)
+                    # Save image
+                    img_saved = save_image(out_filename, output_img)
+
+    except ValueError:
+        print('Input must be a valid image, video, or directory.')
+    
+    # Save processing steps
+    if args.save_steps:
+        # Set image output height
+        output_height = cfg['image']['img_steps_height']
+        # Set output filename
+        steps_base = os.path.splitext(cfg['image']['output_steps'])[0]
+        steps_filename = os.path.join(args.output, f"{steps_base}_bf")
+        # Save file
+        save_steps(steps_filename, img_steps, output_height)
+
+    # End measuring time
+    toc = time.perf_counter()
+    print(f"Operation ran in {toc - tic:0.4f} seconds")
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
